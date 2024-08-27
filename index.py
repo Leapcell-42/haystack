00a766c58d4ba3ce6f1517ca32550506a9785ea7
Leapcell-42 <sjxtem@gmail.com>
2024-08-28 03:41:31 +0800

    Update index.py

--- index.py
+++ index.py
@@ -3,9 +3,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/sherlock")
 def sherlock(q: str):
     names = q.split(' ')
     result = subprocess.run(['sherlock'] + names,
                             capture_output=True, text=True)
     return {"result": result.stdout}
+
+@app.get("/")
+def main():
+    return "success"
