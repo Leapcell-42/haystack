bb481b257c92df90af471874a037ed2d25272cd7
Leapcell-42 <sjxtem@gmail.com>
2024-08-28 04:04:01 +0800

    Update index.py

--- index.py
+++ index.py
@@ -6,8 +6,7 @@ app = FastAPI()
 @app.get("/sherlock")
 def sherlock(q: str):
     name = q.split(' ')[0]
-    subprocess.run(['sherlock'] + name,
-                            capture_output=True, text=True)
+    subprocess.run(['sherlock', name], capture_output=True, text=True)
     with open(f'{name}.txt', 'r') as f:
         result = f.read()
     return {"result": result}
